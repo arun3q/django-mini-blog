cb373713c2bbe0ae95d06f6a6c15a56a4b50ce31
Arun <arun3q@gmail.com>
2018-09-02 21:23:30 +0530

    added authentication functions

--- idea/views.py
+++ idea/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
+from .models import Idea
 
-
-posts = [
-	{
-		'author': "Arun",
-		'title': "Idea one",
-		'content': "My first idea just to test the app.",
-		'date_posted': 'September 1, 2018'
-	},
-	{
-		'author': "Avinash",
-		'title': "Idea Two",
-		'content': "My Second idea just to test the app.",
-		'date_posted': 'September 1, 2018'
-	},
-	{
-		'author': "Amit",
-		'title': "Idea second",
-		'content': "My 3rd idea just to test the app.",
-		'date_posted': 'September 1, 2018'
-	},
-]
 
 
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Idea.objects.all()
 	}
 	return render(request, "idea/home.html", context)
 
--- ideaportal/settings.py
+++ ideaportal/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'idea.apps.IdeaConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'idea-home'
+LOGIN_URL = 'login'
